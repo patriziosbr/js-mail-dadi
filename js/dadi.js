da0f975ccbe417fc7c645db05a26b48338604345
patriziosbr <sbrozzi.patrizio@gmail.com>
2021-04-28 16:36:58 +0200

    dadi ok

--- js/dadi.js
+++ js/dadi.js
@@ -1,4 +1,15 @@
-var user;
-var opponent;
-var rndmNumber = Math.floor(Math.random() * 5) + 1;
-console.log(rndmNumber); 
+var user = Math.floor(Math.random() * 6) + 1;
+console.log(user);
+document.getElementById('user').innerHTML = "User number " + user;
+
+var opponent = Math.floor(Math.random() * 6) + 1;
+console.log(opponent);
+document.getElementById('opponent').innerHTML = "Opponent number " + opponent;
+
+if (user > opponent) {
+    document.getElementById('result').innerHTML = "User higher";
+} else if (user < opponent) {
+    document.getElementById('result').innerHTML = "Opponent higher";
+} else {
+    document.getElementById('result').innerHTML = "Even";
+}
